f648a8cc80c8475882f55b953fde9c02d9155791
Alexander Gabriel <alex.barbalex@gmail.com>
2022-12-28 21:58:34 +0100

    configure

--- rename_js_files.sh
+++
@@ -1,2 +0,0 @@
-# find src -name "*.js" -exec sh -c 'mv "$0" "${0%.js}.ts"' {} \;
-find src/components/Ekf -name "*.ts" -exec sh -c 'mv "$0" "${0%.ts}.tsx"' {} \;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,57 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
+import svgrPlugin from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+        // ...svgr options (https://react-svgr.com/docs/options/)
+      },
+    }),
+    VitePWA({
+      workbox: {
+        sourcemap: true,
+      },
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.svg',
+        'favicon.ico',
+        'robots.txt',
+        'apple-touch-icon.png',
+      ],
+      // https://developer.mozilla.org/en-US/docs/Web/Manifest
+      manifest: {
+        scope: '.',
+        name: 'apflora.ch',
+        short_name: 'apflora',
+        // https://web.dev/add-manifest/:
+        // Your start_url should direct the user straight into your app,
+        // rather than a product landing page.
+        // Think about what the user will want to do once they open your app,
+        // and place them there
+        start_url: './Daten',
+        background_color: '#2e7d32',
+        theme_color: '#2e7d32',
+        display: 'minimal-ui',
+        icon: 'src/images/ophr.png',
+        // not using maskable icon as that can not be transparent
+        // which looks hideous in browser
+        //icon_options: {
+        //  purpose: `any maskable`,
+        //},
+        include_favicon: true,
+        lang: 'de-CH',
+        orientation: 'portrait',
+        description: 'Aktionspläne für Flora-Projekte',
+      },
+      devOptions: {
+        //enabled: true,
+      },
+    }),
+    react(),
+  ],
 })
