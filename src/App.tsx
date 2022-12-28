8563058152e18e2f7aa008a2b1997504723037bc
Alexander Gabriel <alex.barbalex@gmail.com>
2022-12-28 20:00:15 +0100

    migrate

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 // importing isomorphic-fetch is essential
 // otherwise apollo errors during the build
 // see: https://github.com/gatsbyjs/gatsby/issues/11225#issuecomment-457211628
-import 'isomorphic-fetch'
 import queryString from 'query-string'
 
 import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles'
