a95839399e48ff47a3ddf741516662df73f4c290
Alexander Gabriel <alex.barbalex@gmail.com>
2022-12-28 17:54:18 +0100

    initialize

--- rename_js_files.sh
+++ rename_js_files.sh
@@ -1,1 +1,3 @@
-find src -name "*.js" -exec sh -c 'mv "$0" "${0%.js}.ts"' {} \;
+# find src -name "*.js" -exec sh -c 'mv "$0" "${0%.js}.ts"' {} \;
+find src/components/Projekte/TreeContainer/contextmenu -name "*.tx" -exec sh -c 'mv "$0" "${0%.tx}.tsx"' {} \;
+# find src/components/Projekte/TreeContainer/contextmenu -name "*.ts" -exec sh -c 'mv "$0" "${0%.ts}.tsx"' {} \;
