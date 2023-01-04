fe07ea2e82f2db3621c60862c43470a5b9084884
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2023-01-04 11:58:11 +0100

    refactor routing

--- src/components/Projekte/ProjektContainer.tsx
+++ src/components/Projekte/ProjektContainer.tsx
@@ -1,6 +1,19 @@
-import React, { useContext, useRef } from 'react'
+/**
+ * 2020.03.16:
+ * used to build nodes in TreeContainer
+ * but need to pass them to Daten
+ * and react would not like this to happen from below
+ * so needed to move building nodes up to here
+ */
+import React, { useContext, useRef, useMemo, useState, useEffect } from 'react'
 import styled from '@emotion/styled'
 import { observer } from 'mobx-react-lite'
+import { getSnapshot } from 'mobx-state-tree'
+import { useApolloClient } from '@apollo/client'
+import { useQuery } from '@tanstack/react-query'
+import { useParams } from 'react-router-dom'
+import intersection from 'lodash/intersection'
+import jwtDecode from 'jwt-decode'
 
 import Karte from './Karte'
 import TreeContainer from './TreeContainer'
@@ -12,6 +25,9 @@ import ApberForApFromAp from '../Print/ApberForApFromAp'
 import ApberForYear from '../Print/ApberForYear'
 import getActiveForm from '../../modules/getActiveForm'
 import StyledSplitPane from '../shared/StyledSplitPane'
+import buildTreeQueryVariables from './buildTreeQueryVariables'
+import queryTree from './queryTree'
+import buildNodes from './TreeContainer/nodes'
 
 const Container = styled.div`
   height: 100%;
@@ -29,15 +45,200 @@ const InnerContainer = styled.div`
   height: 100%;
 `
 
-const ProjektContainer = ({
-  treeName,
-  tabs: tabsPassed,
-  nodes,
-  treeLoading,
-}) => {
+const tree2TabValues = ['tree2', 'daten2', 'filter2', 'karte2']
+
+const ProjektContainer = ({ treeName }) => {
+  const client = useApolloClient()
   const store = useContext(storeContext)
-  const { isPrint } = store
+  const { isPrint, urlQuery, user } = store
   const { activeNodeArray } = store[treeName]
+  const { projId, artId } = useParams()
+  // react hooks 'exhaustive-deps' rule wants to move treeTabValues into own useMemo
+  // to prevent it from causing unnessecary renders
+  // BUT: this prevents necessary renders: clicking tabs does not cause re-render!
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const treeTabValues = [
+    'tree',
+    'daten',
+    'filter',
+    'karte',
+    ...(projId ? ['exporte'] : []),
+  ]
+
+  const { projekteTabs } = urlQuery
+  const treeTabs = useMemo(
+    () => intersection(treeTabValues, projekteTabs),
+    [projekteTabs, treeTabValues],
+  )
+  const tree2Tabs = intersection(tree2TabValues, projekteTabs)
+
+  const { token } = user
+  const role = token ? jwtDecode(token).role : null
+
+  const treeDataFilter = getSnapshot(store.tree.dataFilter)
+  const treeNodeLabelFilter = getSnapshot(store.tree.nodeLabelFilter)
+  const treeOpenNodes = getSnapshot(store.tree.openNodes)
+  const treeApFilter = store.tree.apFilter
+  const tree2DataFilter = getSnapshot(store.tree2.dataFilter)
+  const tree2NodeLabelFilter = getSnapshot(store.tree2.nodeLabelFilter)
+  const tree2OpenNodes = getSnapshot(store.tree2.openNodes)
+  const tree2ApFilter = store.tree2.apFilter
+  const popGqlFilterTree = store.tree.popGqlFilter
+  const popGqlFilterTree2 = store.tree2.popGqlFilter
+  const apGqlFilterTree = store.tree.apGqlFilter
+  const apGqlFilterTree2 = store.tree2.apGqlFilter
+  const tpopGqlFilterTree = store.tree.tpopGqlFilter
+  const tpopGqlFilterTree2 = store.tree2.tpopGqlFilter
+  const tpopmassnGqlFilterTree = store.tree.tpopmassnGqlFilter
+  const tpopmassnGqlFilterTree2 = store.tree2.tpopmassnGqlFilter
+  const ekGqlFilterTree = store.tree.ekGqlFilter
+  const ekGqlFilterTree2 = store.tree2.ekGqlFilter
+  const ekfGqlFilterTree = store.tree.ekfGqlFilter
+  const ekfGqlFilterTree2 = store.tree2.ekfGqlFilter
+  const beobGqlFilterTree = store.tree.beobGqlFilter
+  const beobGqlFilterTree2 = store.tree2.beobGqlFilter
+
+  const {
+    data: data1,
+    error: error1,
+    isLoading: isLoading1,
+  } = useQuery({
+    queryKey: [
+      'treeQuery',
+      treeDataFilter,
+      treeOpenNodes,
+      treeApFilter,
+      treeNodeLabelFilter,
+      artId,
+      popGqlFilterTree,
+      tpopGqlFilterTree,
+      tpopmassnGqlFilterTree,
+      ekGqlFilterTree,
+      ekfGqlFilterTree,
+      apGqlFilterTree,
+      beobGqlFilterTree,
+      role,
+      treeName,
+    ],
+    queryFn: () =>
+      treeName === 'tree'
+        ? client.query({
+            query: queryTree,
+            variables: buildTreeQueryVariables({
+              dataFilter: treeDataFilter,
+              openNodes: treeOpenNodes,
+              apFilter: treeApFilter,
+              nodeLabelFilter: treeNodeLabelFilter,
+              artId,
+              popGqlFilter: popGqlFilterTree,
+              tpopGqlFilter: tpopGqlFilterTree,
+              tpopmassnGqlFilter: tpopmassnGqlFilterTree,
+              ekGqlFilter: ekGqlFilterTree,
+              ekfGqlFilter: ekfGqlFilterTree,
+              apGqlFilter: apGqlFilterTree,
+              beobGqlFilter: beobGqlFilterTree,
+            }),
+          })
+        : {},
+  })
+
+  const {
+    data: data2,
+    error: error2,
+    isLoading: isLoading2,
+  } = useQuery({
+    queryKey: [
+      'tree2Query',
+      tree2DataFilter,
+      tree2OpenNodes,
+      tree2ApFilter,
+      tree2NodeLabelFilter,
+      artId,
+      popGqlFilterTree2,
+      tpopGqlFilterTree2,
+      tpopmassnGqlFilterTree2,
+      ekGqlFilterTree2,
+      ekfGqlFilterTree2,
+      apGqlFilterTree2,
+      beobGqlFilterTree2,
+      role,
+      treeName,
+    ],
+    queryFn: async () =>
+      treeName === 'tree2'
+        ? client.query({
+            query: queryTree,
+            variables: buildTreeQueryVariables({
+              dataFilter: tree2DataFilter,
+              openNodes: tree2OpenNodes,
+              apFilter: tree2ApFilter,
+              nodeLabelFilter: tree2NodeLabelFilter,
+              artId,
+              popGqlFilter: popGqlFilterTree2,
+              tpopGqlFilter: tpopGqlFilterTree2,
+              tpopmassnGqlFilter: tpopmassnGqlFilterTree2,
+              ekGqlFilter: ekGqlFilterTree2,
+              ekfGqlFilter: ekfGqlFilterTree2,
+              apGqlFilter: apGqlFilterTree2,
+              beobGqlFilter: beobGqlFilterTree2,
+            }),
+          })
+        : {},
+  })
+  const tree1Data = data1?.data
+  const tree2Data = data2?.data
+
+  const [treeNodes, setTreeNodes] = useState([])
+  const [tree2Nodes, setTree2Nodes] = useState([])
+
+  useEffect(() => {
+    //console.log('Projekte, building treeNodes')
+    if (!isLoading1) {
+      setTreeNodes(
+        buildNodes({
+          treeName: 'tree',
+          role,
+          data: tree1Data,
+          loading: isLoading1,
+          store,
+        }),
+      )
+    }
+  }, [
+    isLoading1,
+    store.tree.openNodes,
+    store.tree.openNodes.length,
+    tree1Data,
+    treeDataFilter,
+    role,
+    store,
+  ])
+  useEffect(() => {
+    if (!(tree2Tabs.length === 0 || isPrint)) {
+      //console.log('Projekte, building tree2Nodes')
+      if (!isLoading2) {
+        setTree2Nodes(
+          buildNodes({
+            treeName: 'tree2',
+            role,
+            data: tree2Data,
+            loading: isLoading2,
+            store,
+          }),
+        )
+      }
+    }
+  }, [
+    store.tree2.openNodes,
+    store.tree2.openNodes.length,
+    tree2Data,
+    tree2DataFilter,
+    role,
+    store,
+    tree2Tabs.length,
+    isPrint,
+    isLoading2,
+  ])
 
   const showApberForArt =
     activeNodeArray.length === 7 &&
@@ -54,7 +255,10 @@ const ProjektContainer = ({
   const filterEl = useRef(null)
 
   // remove 2 to treat all same
-  const tabs = [...tabsPassed].map((t) => t.replace('2', ''))
+  let tabs = treeName === 'tree' ? treeTabs : tree2Tabs
+  tabs = [...tabs].map((t) => t.replace('2', ''))
+  const nodes = treeName === 'tree' ? treeNodes : tree2Nodes
+  const treeLoading = treeName === 'tree' ? isLoading1 : isLoading2
 
   const elObj = {
     tree: (
@@ -70,7 +274,11 @@ const ProjektContainer = ({
       <InnerContainer ref={datenEl}>
         <Daten
           treeName={treeName}
-          activeForm={getActiveForm({ store, treeName, nodes })}
+          activeForm={getActiveForm({
+            store,
+            treeName,
+            nodes: nodes,
+          })}
         />
       </InnerContainer>
     ),
@@ -138,7 +346,11 @@ const ProjektContainer = ({
     return (
       <Daten
         treeName={treeName}
-        activeForm={getActiveForm({ store, treeName, nodes })}
+        activeForm={getActiveForm({
+          store,
+          treeName,
+          nodes: nodes,
+        })}
       />
     )
   }
--- src/components/Projekte/index.tsx
+++ src/components/Projekte/index.tsx
@@ -1,27 +1,12 @@
-/**
- * 2020.03.16:
- * used to build nodes in TreeContainer
- * but need to pass them to Daten
- * and react would not like this to happen from below
- * so needed to move building nodes up to here
- */
-import React, { useContext, useMemo, useState, useEffect } from 'react'
+import React, { useContext } from 'react'
 import styled from '@emotion/styled'
 import intersection from 'lodash/intersection'
 import { observer } from 'mobx-react-lite'
-import jwtDecode from 'jwt-decode'
-import { useApolloClient } from '@apollo/client'
-import { useQuery } from '@tanstack/react-query'
-import { getSnapshot } from 'mobx-state-tree'
-import { useParams } from 'react-router-dom'
 
 // when Karte was loaded async, it did not load,
 // but only in production!
 import ProjektContainer from './ProjektContainer'
 import storeContext from '../../storeContext'
-import buildTreeQueryVariables from './buildTreeQueryVariables'
-import queryTree from './queryTree'
-import buildNodes from './TreeContainer/nodes'
 import StyledSplitPane from '../shared/StyledSplitPane'
 
 const Container = styled.div`
@@ -37,203 +22,18 @@ const Container = styled.div`
 const tree2TabValues = ['tree2', 'daten2', 'filter2', 'karte2']
 
 const Projekte = () => {
-  const client = useApolloClient()
   const store = useContext(storeContext)
-  const { isPrint, urlQuery, user } = store
-  const { projId, artId } = useParams()
-  // react hooks 'exhaustive-deps' rule wants to move treeTabValues into own useMemo
-  // to prevent it from causing unnessecary renders
-  // BUT: this prevents necessary renders: clicking tabs does not cause re-render!
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const treeTabValues = [
-    'tree',
-    'daten',
-    'filter',
-    'karte',
-    ...(projId ? ['exporte'] : []),
-  ]
+  const { isPrint, urlQuery } = store
 
   const { projekteTabs } = urlQuery
-  const treeTabs = useMemo(
-    () => intersection(treeTabValues, projekteTabs),
-    [projekteTabs, treeTabValues],
-  )
   const tree2Tabs = intersection(tree2TabValues, projekteTabs)
-
-  const { token } = user
-  const role = token ? jwtDecode(token).role : null
-
-  const treeDataFilter = getSnapshot(store.tree.dataFilter)
-  const treeNodeLabelFilter = getSnapshot(store.tree.nodeLabelFilter)
-  const treeOpenNodes = getSnapshot(store.tree.openNodes)
-  const treeApFilter = store.tree.apFilter
-  const tree2DataFilter = getSnapshot(store.tree2.dataFilter)
-  const tree2NodeLabelFilter = getSnapshot(store.tree2.nodeLabelFilter)
-  const tree2OpenNodes = getSnapshot(store.tree2.openNodes)
-  const tree2ApFilter = store.tree2.apFilter
-  const popGqlFilterTree = store.tree.popGqlFilter
-  const popGqlFilterTree2 = store.tree2.popGqlFilter
-  const apGqlFilterTree = store.tree.apGqlFilter
-  const apGqlFilterTree2 = store.tree2.apGqlFilter
-  const tpopGqlFilterTree = store.tree.tpopGqlFilter
-  const tpopGqlFilterTree2 = store.tree2.tpopGqlFilter
-  const tpopmassnGqlFilterTree = store.tree.tpopmassnGqlFilter
-  const tpopmassnGqlFilterTree2 = store.tree2.tpopmassnGqlFilter
-  const ekGqlFilterTree = store.tree.ekGqlFilter
-  const ekGqlFilterTree2 = store.tree2.ekGqlFilter
-  const ekfGqlFilterTree = store.tree.ekfGqlFilter
-  const ekfGqlFilterTree2 = store.tree2.ekfGqlFilter
-  const beobGqlFilterTree = store.tree.beobGqlFilter
-  const beobGqlFilterTree2 = store.tree2.beobGqlFilter
-
-  const {
-    data: data1,
-    error: error1,
-    isLoading: isLoading1,
-  } = useQuery({
-    queryKey: [
-      'treeQuery',
-      treeDataFilter,
-      treeOpenNodes,
-      treeApFilter,
-      treeNodeLabelFilter,
-      artId,
-      popGqlFilterTree,
-      tpopGqlFilterTree,
-      tpopmassnGqlFilterTree,
-      ekGqlFilterTree,
-      ekfGqlFilterTree,
-      apGqlFilterTree,
-      beobGqlFilterTree,
-      role,
-    ],
-    queryFn: () =>
-      client.query({
-        query: queryTree,
-        variables: buildTreeQueryVariables({
-          dataFilter: treeDataFilter,
-          openNodes: treeOpenNodes,
-          apFilter: treeApFilter,
-          nodeLabelFilter: treeNodeLabelFilter,
-          artId,
-          popGqlFilter: popGqlFilterTree,
-          tpopGqlFilter: tpopGqlFilterTree,
-          tpopmassnGqlFilter: tpopmassnGqlFilterTree,
-          ekGqlFilter: ekGqlFilterTree,
-          ekfGqlFilter: ekfGqlFilterTree,
-          apGqlFilter: apGqlFilterTree,
-          beobGqlFilter: beobGqlFilterTree,
-        }),
-      }),
-  })
-
-  const {
-    data: data2,
-    error: error2,
-    isLoading: isLoading2,
-  } = useQuery({
-    queryKey: [
-      'tree2Query',
-      tree2DataFilter,
-      tree2OpenNodes,
-      tree2ApFilter,
-      tree2NodeLabelFilter,
-      artId,
-      popGqlFilterTree2,
-      tpopGqlFilterTree2,
-      tpopmassnGqlFilterTree2,
-      ekGqlFilterTree2,
-      ekfGqlFilterTree2,
-      apGqlFilterTree2,
-      beobGqlFilterTree2,
-      role,
-    ],
-    queryFn: async () =>
-      client.query({
-        query: queryTree,
-        variables: buildTreeQueryVariables({
-          dataFilter: tree2DataFilter,
-          openNodes: tree2OpenNodes,
-          apFilter: tree2ApFilter,
-          nodeLabelFilter: tree2NodeLabelFilter,
-          artId,
-          popGqlFilter: popGqlFilterTree2,
-          tpopGqlFilter: tpopGqlFilterTree2,
-          tpopmassnGqlFilter: tpopmassnGqlFilterTree2,
-          ekGqlFilter: ekGqlFilterTree2,
-          ekfGqlFilter: ekfGqlFilterTree2,
-          apGqlFilter: apGqlFilterTree2,
-          beobGqlFilter: beobGqlFilterTree2,
-        }),
-      }),
-  })
-  const treeData = data1?.data
-  const tree2Data = data2?.data
-
-  const [treeNodes, setTreeNodes] = useState([])
-  const [tree2Nodes, setTree2Nodes] = useState([])
-
-  useEffect(() => {
-    //console.log('Projekte, building treeNodes')
-    if (!isLoading1) {
-      setTreeNodes(
-        buildNodes({
-          treeName: 'tree',
-          role,
-          data: treeData,
-          loading: isLoading1,
-          store,
-        }),
-      )
-    }
-  }, [
-    isLoading1,
-    store.tree.openNodes,
-    store.tree.openNodes.length,
-    treeData,
-    treeDataFilter,
-    role,
-    store,
-  ])
-  useEffect(() => {
-    if (!(tree2Tabs.length === 0 || isPrint)) {
-      //console.log('Projekte, building tree2Nodes')
-      if (!isLoading2) {
-        setTree2Nodes(
-          buildNodes({
-            treeName: 'tree2',
-            role,
-            data: tree2Data,
-            loading: isLoading2,
-            store,
-          }),
-        )
-      }
-    }
-  }, [
-    store.tree2.openNodes,
-    store.tree2.openNodes.length,
-    tree2Data,
-    tree2DataFilter,
-    role,
-    store,
-    tree2Tabs.length,
-    isPrint,
-    isLoading2,
-  ])
 
   console.log('Projekte rendering')
 
   if (tree2Tabs.length === 0 || isPrint) {
     return (
       <Container>
-        <ProjektContainer
-          treeName="tree"
-          tabs={treeTabs}
-          nodes={treeNodes}
-          treeLoading={isLoading1}
-          treeError={error1}
-        />
+        <ProjektContainer treeName="tree" />
       </Container>
     )
   }
@@ -241,20 +41,8 @@ const Projekte = () => {
   return (
     <Container>
       <StyledSplitPane split="vertical" defaultSize="50%">
-        <ProjektContainer
-          treeName="tree"
-          tabs={treeTabs}
-          nodes={treeNodes}
-          treeLoading={isLoading1}
-          treeError={error1}
-        />
-        <ProjektContainer
-          treeName="tree2"
-          tabs={tree2Tabs}
-          nodes={tree2Nodes}
-          treeLoading={isLoading2}
-          treeError={error2}
-        />
+        <ProjektContainer treeName="tree" />
+        <ProjektContainer treeName="tree2" />
       </StyledSplitPane>
     </Container>
   )
