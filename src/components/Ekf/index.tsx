1aaa54e1754c45a89b1fdd4076dc24a1cb2bc5c7
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2023-01-04 10:33:59 +0100

    modularize

--- src/components/Ekf/index.tsx
+++ src/components/Ekf/index.tsx
@@ -1,6 +1,5 @@
 import React, { useContext, useRef } from 'react'
 import styled from '@emotion/styled'
-import SplitPane from 'react-split-pane'
 import jwtDecode from 'jwt-decode'
 import { observer } from 'mobx-react-lite'
 
@@ -9,6 +8,7 @@ import { observer } from 'mobx-react-lite'
 import EkfList from './ListContainer'
 import Tpopfreiwkontr from '../Projekte/Daten/Tpopfreiwkontr'
 import storeContext from '../../storeContext'
+import StyledSplitPane from '../shared/StyledSplitPane'
 
 const Container = styled.div`
   height: 100%;
@@ -17,41 +17,6 @@ const Container = styled.div`
   @media print {
     display: block;
     height: auto !important;
-  }
-`
-const StyledSplitPane = styled(SplitPane)`
-  .Resizer {
-    background: #388e3c;
-    opacity: 1;
-    -moz-box-sizing: border-box;
-    -webkit-box-sizing: border-box;
-    box-sizing: border-box;
-  }
-
-  .Resizer:hover {
-    -webkit-transition: all 2s ease;
-    transition: all 2s ease;
-  }
-
-  .Resizer.vertical {
-    border-left: 3px solid #388e3c;
-    /*border-right: 1.5px solid #388e3c;*/
-    cursor: col-resize;
-    background-color: #388e3c;
-  }
-
-  .Resizer.vertical:hover {
-    border-left: 2px solid rgba(0, 0, 0, 0.3);
-    border-right: 2px solid rgba(0, 0, 0, 0.3);
-  }
-  .Resizer.disabled {
-    cursor: not-allowed;
-  }
-  .Resizer.disabled:hover {
-    border-color: transparent;
-  }
-  .Pane {
-    overflow: hidden;
   }
 `
 const InnerContainer = styled.div`
