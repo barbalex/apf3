5bdbf7e458921de5ddf64aa0e2a083de2978e7b4
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2023-01-05 12:26:22 +0100

    clean and comment

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -1,6 +1,4 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
-// import { useContext } from 'react'
-import { observer } from 'mobx-react-lite'
 
 import Home from '../Home'
 import EkPlan from '../EkPlan'
@@ -10,35 +8,32 @@ import Docs from '../Docs'
 import ProtectedRoute from './ProtectedRoute'
 import Ekf from '../Ekf'
 import Projekte from '../Projekte'
-// import storeContext from '../../storeContext'
 // import Unterhalt from './components/Unterhalt'
 const ekfRefYear = new Date().getFullYear()
 
 // uncommeent unterhalt route for Unterhalt
-const RouterComponent = () => {
-  // const store = useContext(storeContext)
+// DANGER
+// Anything using path variables does not work in tree2!
+const RouterComponent = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/Daten/*" element={<ProtectedRoute />}>
+      {/* <Route path="*" element={<Unterhalt />}></Route> */}
+      <Route path="*" element={<Projekte />} />
+      <Route path="Projekte/:projId/EK-Planung" element={<EkPlan />} />
+      <Route path="Benutzer/:userId/*" element={<Projekte />} />
+      <Route
+        path="Benutzer/:userId/EKF/*"
+        element={<Navigate to={ekfRefYear.toString()} />}
+      />
+      <Route path="Benutzer/:userId/EKF/:ekfYear/*" element={<Ekf />} />
+      <Route path="Benutzer/:userId/EKF/:ekfYear/:ekfId" element={<Ekf />} />
+    </Route>
+    <Route path="/Dokumentation/*" element={<Docs />}>
+      {DocRoutes()}
+    </Route>
+    <Route path="*" element={<FourOhFour />} />
+  </Routes>
+)
 
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/Daten/*" element={<ProtectedRoute />}>
-        {/* <Route path="*" element={<Unterhalt />}></Route> */}
-        <Route path="*" element={<Projekte />} />
-        <Route path="Projekte/:projId/EK-Planung" element={<EkPlan />} />
-        <Route path="Benutzer/:userId/*" element={<Projekte />} />
-        <Route
-          path="Benutzer/:userId/EKF/*"
-          element={<Navigate to={ekfRefYear.toString()} />}
-        />
-        <Route path="Benutzer/:userId/EKF/:ekfYear/*" element={<Ekf />} />
-        <Route path="Benutzer/:userId/EKF/:ekfYear/:ekfId" element={<Ekf />} />
-      </Route>
-      <Route path="/Dokumentation/*" element={<Docs />}>
-        {DocRoutes()}
-      </Route>
-      <Route path="*" element={<FourOhFour />} />
-    </Routes>
-  )
-}
-
-export default observer(RouterComponent)
+export default RouterComponent
