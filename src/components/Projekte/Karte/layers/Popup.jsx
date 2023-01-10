170dee51411f232bf73240526d8a9c005fc07969
Alexander Gabriel <alex.barbalex@gmail.com>
2023-01-10 22:59:18 +0100

    clean

--- src/components/Projekte/Karte/layers/Popup.jsx
+++ src/components/Projekte/Karte/layers/Popup.jsx
@@ -4,62 +4,55 @@ import Linkify from 'react-linkify'
 // Somehow emotions styles were not applied
 // So need to style inline
 
-const Popup = ({ layersData, mapSize = { x: 1000, y: 1000 } }) => {
-  // console.log('Popup', { mapSize, x: mapSize.x, y: mapSize.y })
-
-  return (
-    <div
-      style={{
-        overflow: 'auto',
-        maxHeight: `${mapSize.y - 40}px`,
-        maxWidth: `${mapSize.x - 60}px`,
-        marginRight: '-5px',
-      }}
-    >
-      {layersData.map((ld, index) => {
-        console.log('Popup, label:', ld.label)
-        return (
+const Popup = ({ layersData, mapSize = { x: 1000, y: 1000 } }) => (
+  <div
+    style={{
+      overflow: 'auto',
+      maxHeight: `${mapSize.y - 40}px`,
+      maxWidth: `${mapSize.x - 60}px`,
+      marginRight: '-5px',
+    }}
+  >
+    {layersData.map((ld, index) => (
+      <div
+        key={ld.label}
+        style={{
+          marginTop: index === 0 ? 0 : 15,
+        }}
+      >
+        <h4 style={{ marginTop: 0, marginBottom: 10 }}>{ld.label}</h4>
+        {ld.properties.map(([key, value], index) => (
           <div
-            key={ld.label}
+            key={`${key}/${index}`}
             style={{
-              marginTop: index === 0 ? 0 : 15,
+              display: 'grid',
+              gridTemplateColumns: '85px 1fr',
+              fontSize: 'x-small !important',
+              backgroundColor:
+                index % 2 === 0 ? 'rgba(0, 0, 0, 0.05)' : 'white',
+              color: index % 2 === 0 ? 'black' : 'inherit',
             }}
           >
-            <h4 style={{ marginTop: 0, marginBottom: 10 }}>{ld.label}</h4>
-            {ld.properties.map(([key, value], index) => (
-              <div
-                key={`${key}/${index}`}
-                style={{
-                  display: 'grid',
-                  gridTemplateColumns: '85px 1fr',
-                  fontSize: 'x-small !important',
-                  backgroundColor:
-                    index % 2 === 0 ? 'rgba(0, 0, 0, 0.05)' : 'white',
-                  color: index % 2 === 0 ? 'black' : 'inherit',
-                }}
-              >
-                <div
-                  style={{
-                    color: 'rgba(0, 0, 0, 0.6)',
-                    overflowWrap: 'anywhere',
-                  }}
-                >{`${key}:`}</div>
-                <Linkify
-                  componentDecorator={(decoratedHref, decoratedText, key) => (
-                    <a target="blank" href={decoratedHref} key={key}>
-                      {decoratedText}
-                    </a>
-                  )}
-                >
-                  <div style={{ overflowWrap: 'anywhere' }}>{value}</div>
-                </Linkify>
-              </div>
-            ))}
+            <div
+              style={{
+                color: 'rgba(0, 0, 0, 0.6)',
+                overflowWrap: 'anywhere',
+              }}
+            >{`${key}:`}</div>
+            <Linkify
+              componentDecorator={(decoratedHref, decoratedText, key) => (
+                <a target="blank" href={decoratedHref} key={key}>
+                  {decoratedText}
+                </a>
+              )}
+            >
+              <div style={{ overflowWrap: 'anywhere' }}>{value}</div>
+            </Linkify>
           </div>
-        )
-      })}
-    </div>
-  )
-}
+        ))}
+      </div>
+    ))}
+  </div>
+)
 
 export default Popup
